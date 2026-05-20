63ba0895762f16e297eaab47d71fc11b01d8c9b1
JulianImpe <julian.m.imperiale@gmail.com>
2026-05-20 20:45:06 -0300

    primer commit - estructura inicial

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Administracion_Consorcio_PW3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
